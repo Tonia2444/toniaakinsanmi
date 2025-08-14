e173ffa59374404fdbf71b2fab373769cc30d1f9
tonia <feyisayoakinsanmi48@gmail.com>
2025-08-14 20:10:27 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/toniaakinsanmi/',
+  base: "/tonia-portfolio",
   plugins: [
     tailwindcss(), react()
   ],
