2c9851a76b6fa9a7fdd9b97d1f5fa03fa5f209b1
tonia <feyisayoakinsanmi48@gmail.com>
2025-08-14 20:36:54 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: "/tonia-portfolio",
+  base: "/my-portfolio",
   plugins: [
     tailwindcss(), react()
   ],
